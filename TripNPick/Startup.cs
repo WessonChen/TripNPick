ec158cc9b0cb0d9c989fa2f56f2b823ac803a42b
wche0004 <wche0004@DESKTOP-L3R72A9>
2018-03-27 08:04:28 +1100

    Add project files.

---
+++ TripNPick/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TripNPick.Startup))]
+namespace TripNPick
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
