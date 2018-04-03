6fe094a686c04df208520acac476d4b77f9d607c
wche0004 <wche0004@DESKTOP-L3R72A9>
2018-04-03 22:46:45 +1000

    404 Page Added

--- TripNPick/Global.asax.cs
+++ TripNPick/Global.asax.cs
@@ -17,5 +17,12 @@ namespace TripNPick
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception exc = Server.GetLastError();
+            Server.ClearError();
+            Response.Redirect("/ErrorPage/ErrorMessage");
+        }
     }
 }
