8066a2cf73295e1a120aea9d20494dc2c85238fd
shazeed ahsan <sadzahsan1111@gmail.com>
2018-04-30 18:56:09 +1000

    about us page

---
+++ TripNPick/Models/SuggestionForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TripNPick.Models
+{
+    public class SuggestionForm
+    {
+        [Required(ErrorMessage = "Your First Name is required")]
+        public string userFirstName { get; set; }
+
+        [Required(ErrorMessage = "Your Last Name is required")]
+        public string userLastName { get; set; }
+
+        [Required(ErrorMessage = "Your Email is required"), EmailAddress(ErrorMessage ="Please provide a valid email address.")]
+        public string userEmail { get; set; }
+
+        [Required(ErrorMessage = "Your Suggestion is required")]
+        public string userSuggestion { get; set; }
+    }
+}
